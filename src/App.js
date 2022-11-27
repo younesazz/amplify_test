b9dd58c9445996e9ab580dcdb1573609565c23b0
younesazz <youneshamani88@gmail.com>
2022-11-27 16:55:26 +0100

    five commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import '@aws-amplify/ui-react/styles.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
 
 
-function App({ signOut }) {
+function App({ signOut , user }) {
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
+        <p>{user.attributes.email}</p>
         <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default withAuthenticator(App,{
+  socialProviders : ['google','facebook']
+});
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Amplify} from 'aws-amplify';
 import config from './aws-exports';
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AmplifyProvider>
     <App />
-  </React.StrictMode>
+  </AmplifyProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
